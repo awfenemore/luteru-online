8154551f7d4aecf7e33f7793e54e8d15c35ada45
Adam Fenemore <awfenemore@gmail.com>
2021-12-27 14:59:30 +1300

    ready to load content

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,68 @@
 <template>
   <v-app>
-    test
+    <div class="heading-container">
+      <h1 class="heading">Inspirations</h1>
+      <div class="menu-container">
+        <a
+          class="menu-link"
+          v-for="(item, i) in items"
+          :key="i"
+          :href="'#' + item.title"
+          >{{ item.title }}</a
+        >
+      </div>
+    </div>
+    <div class="content-container">
+      <div
+        class="section"
+        v-for="(item, i) in items"
+        :key="i"
+        :id="'#' + item.title"
+      >
+        <h3>{{ item.title }}</h3>
+        <div class="section-links">
+          <a
+            class="pic-link"
+            v-for="(pic, j) in item.pics"
+            :key="j"
+            :href="pic"
+            target="_blank"
+          >
+            <img v-if="isImage(pic)" :src="pic" class="pic-img" />
+            <img
+              v-else-if="isRedditLink(pic)"
+              src="./assets/reddit-logo.png"
+              class="pic-img"
+            />
+            <h3 v-else>{{ pic }}</h3></a
+          >
+        </div>
+      </div>
+    </div>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import items from "./items";
 
 export default Vue.extend({
   name: "App",
   data() {
-    return {};
+    return {
+      items: items
+    };
   },
-  methods: {}
+  methods: {
+    isImage(url: string): boolean {
+      const lower = url.toLowerCase();
+      return lower.includes(".jpg") || lower.includes(".png");
+    },
+    isRedditLink(url: string): boolean {
+      const lower = url.toLowerCase();
+      return lower.includes("redd");
+    }
+  }
 });
 </script>
 
---
+++ src/items.js
@@ -0,0 +1,18 @@
+const items = [
+  {
+    title: "Art",
+    pics: [
+      "https://i.redd.it/3f7vu7x4mc781.jpg",
+      "https://i.redd.it/3f7vu7x4mc781.jpg",
+      "https://i.redd.it/3f7vu7x4mc781.jpg"
+    ]
+  },
+  {
+    title: "Ideas",
+    pics: [
+      "https://www.reddit.com/r/antiwork/comments/jh1sif/millennials_are_causing_a_baby_bust_what_the/g9upbyl/?context=3"
+    ]
+  }
+];
+
+export default items;
