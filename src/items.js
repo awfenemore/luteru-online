be171f2eec5d09bb3c9cdf11204d015e7a4cb748
Adam Fenemore <awfenemore@gmail.com>
2021-12-27 20:05:24 +1300

    add content

--- src/items.js
+++ src/items.js
@@ -3,14 +3,75 @@ const items = [
     title: "Art",
     pics: [
       "https://i.redd.it/3f7vu7x4mc781.jpg",
-      "https://i.redd.it/3f7vu7x4mc781.jpg",
-      "https://i.redd.it/3f7vu7x4mc781.jpg"
+      "https://i.redd.it/gg39615cke581.jpg",
+      "https://i.redd.it/zmdn688y2s381.png",
+      "https://i.redd.it/ffhi7pfyist31.jpg",
+      "https://i.redd.it/36b8eyh51wh21.jpg",
+      "https://i.redd.it/fqluyfcffnm41.jpg",
+      "https://i.imgur.com/BJWL58X.jpg",
+      "https://i.redd.it/m0qafx13zr961.jpg",
+      "https://i.redd.it/vfdc8ih209661.jpg",
+      "https://i.redd.it/pj8doyaabdn61.jpg",
+      "https://i.redd.it/2bwbjj0njva71.jpg",
+      "https://i.redd.it/9dy9t5sq2ib71.jpg",
+      "https://i.redd.it/zfspgoqbwnf71.jpg",
+      "https://i.redd.it/312d5fp888g71.jpg",
+      "https://i.redd.it/w2bbba6l3cq71.jpg",
+      "https://i.redd.it/cmjuxlv2lvr71.jpg",
+      "https://i.redd.it/g8z6zyuxnis71.jpg",
+      "https://i.redd.it/ened86rtbvv71.jpg",
+      "https://i.redd.it/grrnhb95sux71.jpg",
+      "https://i.redd.it/2hyihk3t69z71.jpg",
+      "https://i.redd.it/23zw04j2t9081.jpg",
+      "https://i.redd.it/rjg3ulsros081.jpg",
+      "https://i.redd.it/ta8dx7jfko381.jpg",
+      "https://i.redd.it/n4yp1sjper481.png",
+      "https://i.redd.it/sy04q065tu481.jpg",
+      "https://i.redd.it/9muj317w16581.jpg",
+      "https://i.redd.it/kispr467qk681.jpg",
+      "https://i.redd.it/mp4bc3d73ey61.jpg",
+      "https://gfycat.com/skeletalvapidfirefly-butterfly-painting",
+      "https://www.reddit.com/r/oddlysatisfying/comments/my2ubv/mandala_string_art/",
+      "https://limitlane.com/wp-content/uploads/photo-gallery/imported_from_media_libray/These-waves-in-Morocco.jpg"
     ]
   },
   {
     title: "Ideas",
     pics: [
-      "https://www.reddit.com/r/antiwork/comments/jh1sif/millennials_are_causing_a_baby_bust_what_the/g9upbyl/?context=3"
+      "https://www.reddit.com/r/antiwork/comments/jh1sif/millennials_are_causing_a_baby_bust_what_the/g9upbyl/?context=3",
+      "https://www.vox.com/2016/7/6/12098122/california-pipevine-swallowtail-butterfly-population",
+      "https://www.goodnewsnetwork.org/largest-seagrass-meadow-restoration-in-the-world-in-virginia/",
+      "https://vjmpublishing.nz/?p=22249",
+      "https://i.imgur.com/EHwJNxm.jpg",
+      "https://i.redd.it/3amzmuyuzp471.jpg",
+      "https://www.reddit.com/gallery/oae3sw",
+      "https://i.redd.it/i7wfz7bw0ej71.jpg",
+      "https://www.reddit.com/gallery/pe8n8w",
+      "https://i.redd.it/89wk5ccvipl71.jpg",
+      "https://www.reddit.com/r/woodworking/comments/q6y8pp/nuts_and_seeds_shelf_i_built_for_my_wife/",
+      "https://i.redd.it/df4e2npnyuv71.jpg",
+      "https://i.redd.it/bxmc0axx9s681.jpg"
+    ]
+  },
+  {
+    title: "Sport",
+    pics: [
+      "https://twitter.com/CricketBadge/status/1453039655079878663",
+      "https://old.reddit.com/r/nba/comments/pvr2si/prime_manute_bol_77_weighed_roughly_the_same/",
+      "https://www.youtube.com/watch?v=ZsDbkFqLjDU&t=65s"
+    ]
+  },
+  {
+    title: "Other",
+    pics: [
+      "https://gfycat.com/franktestycanvasback-lightning-strike-burning-tree-environment",
+      "https://i.redd.it/o66edwwn2td51.jpg",
+      "https://www.reddit.com/r/Damnthatsinteresting/comments/fyg4un/how_this_tesegrity_is_made_by_youlab/",
+      "https://i.redd.it/2h1tqccv6fj71.jpg",
+      "https://www.reddit.com/r/Damnthatsinteresting/comments/pp4ynq/brain_cells_in_a_culture_trying_to_form/",
+      "https://i.redd.it/unn7n6wmjxq71.jpg",
+      "https://i.redd.it/2sjnjrox2xu71.png",
+      "https://i.redd.it/w0fsg5zyi6w71.jpg"
     ]
   }
 ];
