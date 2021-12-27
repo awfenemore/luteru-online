c51cb20a3ddd14de3f6a43fa3aab40ef410179e9
Adam Fenemore <awfenemore@gmail.com>
2021-12-27 13:42:04 +1300

    base

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,14 @@
 <template>
   <v-app>
-    <Header id="top" />
-
-    <div class="top-border"></div>
-
-    <v-main class="content">
-      <router-view></router-view>
-    </v-main>
-
-    <div class="bot-border"></div>
-
-    <Footer />
+    test
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import Footer from "./components/micro/Footer.vue";
-import Header from "./components/micro/Header.vue";
 
 export default Vue.extend({
   name: "App",
-  head: {
-    meta: [{ name: "theme-color", content: "#54850A" }]
-  },
-  components: {
-    Footer,
-    Header
-  },
   data() {
     return {};
   },
@@ -36,4 +17,3 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" src="./styles/App.scss"></style>
-<style lang="scss" src="./styles/micro/shared.scss"></style>
