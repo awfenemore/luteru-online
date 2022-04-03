4aa2591a3ec09f2f436d3429dbf22b016ec997a6
Adam Fenemore <awfenemore@gmail.com>
2022-04-03 20:42:34 +1200

    write letter

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,165 @@
 <template>
   <v-app>
-    <div class="heading-container">
-      <h1 class="heading">Inspirations</h1>
-      <div class="menu-container">
-        <a
-          class="menu-link"
-          v-for="(item, i) in items"
-          :key="i"
-          :href="'#' + item.title"
-          >{{ item.title }}</a
-        >
-      </div>
-    </div>
-    <div class="content-container">
-      <div
-        class="section"
-        v-for="(item, i) in items"
-        :key="i"
-        :id="'#' + item.title"
+    <div class="svg-container">
+      <svg
+        width="500"
+        height="500"
+        viewBox="0 0 500 500"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <h3>{{ item.title }}</h3>
-        <div class="section-links">
-          <a
-            class="pic-link"
-            v-for="(pic, j) in item.pics"
-            :key="j"
-            :href="pic"
-            target="_blank"
-          >
-            <img v-if="isImage(pic)" :src="pic" class="pic-img" />
-            <iframe
-              v-else-if="isReddVid(pic)"
-              id="reddit-embed"
-              :src="pic"
-              sandbox="allow-scripts allow-same-origin allow-popups"
-              style="border: none;"
-              height="500"
-              width="500"
-              scrolling="no"
-            ></iframe>
-            <img
-              v-else-if="isRedditLink(pic)"
-              src="./assets/reddit-logo.png"
-              class="pic-img"
-            />
-            <div
-              v-else-if="isGfycat(pic)"
-              style="position:relative; padding-bottom:calc(100.00% + 44px)"
-            >
-              <iframe
-                :src="pic"
-                frameborder="0"
-                scrolling="no"
-                width="100%"
-                height="100%"
-                style="position:absolute;top:0;left:0;"
-                allowfullscreen
-              >
-              </iframe>
-            </div>
-            <h3 v-else>{{ pic }}</h3></a
-          >
-        </div>
-      </div>
+        <text x="250" y="150" class="taylor" text-anchor="middle">TAYLOR</text>
+        <text x="250" y="360" class="three" text-anchor="middle">3</text>
+        <rect x="30" y="0" width="6" height="500" fill="white" />
+        <rect x="470" y="0" width="6" height="500" fill="white" />
+      </svg>
+    </div>
+    <div class="heading-container">
+      <h1>The Luteru Project</h1>
+      <h3>
+        An open letter of love and respect, from a lifelong New Zealand cricket
+        fan to the one and only, Luteru Ross Poutoa Lote Taylor
+      </h3>
+    </div>
+    <div class="text-container">
+      <p class="text">
+        We all knew this day was coming, but I’m still not sure anyone was quite
+        willing to accept it.
+      </p>
+      <p class="text">
+        Some might think you had to field questions about your retirement for a
+        while because of your age. It’s not that Ross. It’s because your shoes
+        will be so hard to fill. They might be impossible to fill. They don’t
+        ask those questions because they’re waiting for you to move on, they ask
+        them because they know that today is the end of an era. The end of our
+        time with an absolute legend of New Zealand cricket, and I just want to
+        say thank you.
+      </p>
+      <p class="text">
+        I am 32 now, and I have been watching cricket my whole life. I have seen
+        your entire career from start to finish, and it truly has been a
+        privilege. I don’t think I was quite old enough to fully appreciate what
+        a world class player looked like for a long time. New Zealand has always
+        been, and probably will always be, a team of cricketing underdogs,
+        competing in a sport against countries that are orders of magnitudes
+        larger than us - by population and often by budget. We struggle to
+        produce truly world class players because of it, and although I saw a
+        lot of McCullum and Vettori and Fleming, I never got to fully appreciate
+        their entire careers.
+      </p>
+      <p class="text">
+        But I did with you Ross. All the way from when I heard about someone
+        coming through the ranks for Central Districts smacking it to all parts,
+        through to the incredible record you have fashioned for yourself in all
+        formats now. Someone who was a bit raw, but was powerful and aggressive.
+        Someone who had the potential to take the next step. Not just to
+        national honours, but someone who could be something special. Someone
+        genuinely world class.
+      </p>
+      <p class="text">
+        At that time, New Zealand just didn’t have anyone who had truly taken
+        that next step. We had good players, we even had players who would go on
+        to be great, but no one was truly world class in all conditions - or,
+        unfortunately, no one that could stay healthy long enough to really
+        prove it. Although we had some great successes, our 6th and 5th place
+        rankings for Tests and ODI’s respectively when you began playing for the
+        Black Caps felt pretty accurate. Today, you are bowing out with the
+        Black Caps as tournament finalists in all three formats, currently
+        ranked 1, 3, and 4, with silverware in the cabinet.
+      </p>
+      <p class="text">
+        What has happened in your career, Ross, is that you have brought about a
+        true golden age of New Zealand cricket. We have world class players in
+        all of our sides, including some of our best ever, and a team that has
+        dominated some of the best in the business consistently, inside and
+        outside New Zealand conditions. The true gift of watching top level
+        sport is watching someone strive for greatness, working hard to be the
+        best they can be. As you have been with New Zealand, you have strived
+        for greatness, and New Zealand has come with you. We are the best we can
+        be, and the best we have ever been, and it has been thanks to you, Ross.
+      </p>
+      <p class="text">
+        In that time, the Black Caps have given us some unbelievable memories.
+        Memories that wouldn;t have happened without you. The team that you have
+        fought for and helped to forge and create, has done things that I wasn’t
+        sure we could ever achieve.
+      </p>
+      <p class="text">
+        I won’t forget watching you hit the winning runs in the World Test
+        Championship final.
+      </p>
+      <p class="text">
+        I won’t forget the 2019 World Cup, including your crucial innings
+        against India, and that very special final.
+      </p>
+      <p class="text">
+        I won’t forget the 2015 World Cup that you were such a key part of. The
+        Southee show against England, the Guptill show against the West Indies,
+        Kane winning one of the best ever games against Australia and looking
+        positively bored while doing it, and I won’t forget Grant Elliott hit
+        the most beautiful six I may ever have the privilege to see. These are
+        your teams Ross, and they are some of the best we have ever had.
+      </p>
+      <p class="text">
+        I certainly won’t forget you going to Australia and hitting an
+        exceptional Australian team for the highest score ever by a visitor to
+        Australia, or coming back to dismantle England for 181* on one leg. You
+        even led the Black Caps to our first win in Australia for 26 years.
+      </p>
+      <p class="text">
+        That side that you helped forge and create even went about setting world
+        records and historical performances that will sit in cricketing history
+        forever. You took a catch when Ajaz Patel did the inconceivable against
+        a great Indian side in their own conditions. You played in the games
+        where we hit both the fastest ever test century in Christchurch, and the
+        fastest ODI century at the time in Queenstown. You got the best seat in
+        the house watching us bowl England out in 94 minutes with the pink ball,
+        still the fastest we have ever bowled a team out. You played in an all
+        time great 2007 Chappell-Hadlee series, where Australia took their only
+        10 wicket loss in ODI’s to date, and helped us chase 335+ in both of the
+        next two games. That team of yours even got the test triple century
+        monkey off our collective backs. You may not have been playing in that
+        game, but that was your team Ross.
+      </p>
+      <p class="text">
+        The record books are simply laden with your influence, individually and
+        as a team, and I hope in time you get to reflect on the impact you have
+        had. You have been a wonderful player and role model, and watching your
+        journey from young gun to New Zealand cricket legend has been fantastic.
+        A true and genuine great, your career has been glorious and it has been
+        inspiring to see. You have become so much, the team has become so much,
+        and it wouldn’t have happened without you. In your time Ross, I have
+        watched you turn the Black Caps into a champion side that has a genuine
+        argument as the best team in the world in any form of cricket. It is a
+        true Golden Age for New Zealand cricket.
+      </p>
+      <p class="text">
+        That will be your legacy Ross. You were instrumental in turning a team
+        with unrealised potential, into the best team we have ever had and an
+        international force. You have been incredible to watch, a gentleman, a
+        servant to the game, a human being, and an inspiration to so many. I
+        hope you enjoy your time after playing, and I sincerely wish you all the
+        best.
+      </p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p class="text">
+        Thank you to your family, thank you to you, and thank you for
+        everything, the one and only, Luteru Ross Poutoa Lote Taylor.
+      </p>
+    </div>
+    <div class="footer">
+      <p style="margin-bottom: 0;">A Fenemore, 2022</p>
+      <a href="mailto:awfenemore@gmail.com">Contact</a>
     </div>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-// @ts-ignore
-import items from "./items";
 
 export default Vue.extend({
-  name: "App",
-  data() {
-    return {
-      items: items
-    };
-  },
-  methods: {
-    isImage(url: string): boolean {
-      const lower = url.toLowerCase();
-      return lower.includes(".jpg") || lower.includes(".png");
-    },
-    isRedditLink(url: string): boolean {
-      const lower = url.toLowerCase();
-      return lower.includes("redd");
-    },
-    isGfycat(url: string): boolean {
-      const lower = url.toLowerCase();
-      return lower.includes("gfycat");
-    },
-    isReddVid(url: string): boolean {
-      const lower = url.toLowerCase();
-      return lower.includes("redditmedia");
-    }
-  }
+  name: "App"
 });
 </script>
 
